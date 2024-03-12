13f74dcbb1389521c4d4e9aa4ee84d797c68cc41
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2024-03-12 02:07:44 -0300

    pass 5st stage

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 // Uncomment this block to pass the first stage
 use anyhow::Result;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::{TcpListener, TcpStream};
+
+use tokio::net::TcpListener;
+
+mod server;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -10,22 +12,9 @@ async fn main() -> Result<()> {
     loop {
         let (stream, _) = listener.accept().await?;
         tokio::spawn(async move {
-            if let Err(e) = handle_stream(stream).await {
+            if let Err(e) = server::handle_stream(stream).await {
                 eprintln!("failed to process connection; error = {:?}", e);
             }
         });
     }
-}
-
-async fn handle_stream(mut stream: TcpStream) -> Result<()> {
-    let mut buf = [0; 512];
-    loop {
-        let n = stream.read(&mut buf).await?;
-        if n == 0 {
-            break;
-        }
-        stream.write_all(b"+PONG\r\n").await?;
-    }
-
-    Ok(())
 }
---
+++ src/server/mod.rs
@@ -0,0 +1,86 @@
+use std::str::FromStr;
+
+use anyhow::Result;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
+
+use self::command::Command;
+
+mod command;
+
+pub async fn handle_stream(mut stream: TcpStream) -> Result<()> {
+    let mut buf = [0; 512];
+    loop {
+        let n = stream.read(&mut buf).await?;
+        if n == 0 {
+            break;
+        }
+        let input = std::str::from_utf8(&buf[..n])?;
+        let (command, args) = match parse_message(input) {
+            Ok((c, a)) => (c, a),
+            Err(e) => {
+                println!("{}", e);
+                stream.write_all(e.as_bytes()).await?;
+                continue;
+            }
+        };
+
+        let response = command::handle_command(command, args);
+        stream.write_all(response.as_bytes()).await?;
+    }
+
+    Ok(())
+}
+
+fn parse_message<'a>(s: &'a str) -> Result<(Command, Vec<&'a str>), String> {
+    let lines: Vec<&str> = s.lines().collect();
+
+    let first_line = match lines.get(0) {
+        Some(line) => *line,
+        None => return Err("No message\r\n".to_string()),
+    };
+
+    if !first_line.contains("*") {
+        return Err("Unknown message format\r\n".to_string());
+    }
+
+    let num_of_args = first_line
+        .chars()
+        .skip(1)
+        .collect::<String>()
+        .parse::<usize>()
+        .map_err(|_| "Invalid number of arguments\r\n".to_string())?;
+
+    let num_of_args = num_of_args * 2;
+
+    if lines.len() - 1 != num_of_args {
+        return Err("Invalid number of arguments\r\n".to_string());
+    }
+
+    let mut args: Vec<&str> = vec![];
+
+    for i in 1..=num_of_args {
+        if i % 2 != 0 {
+            continue;
+        }
+        if lines[i].starts_with("$") {
+            return Err("Invalid argument format\r\n".to_string());
+        }
+        let arg = lines[i];
+        args.push(arg);
+    }
+
+    if args.len() == 0 {
+        return Err("No command\r\n".to_string());
+    }
+
+    let command_str = args[0];
+
+    let command = match Command::from_str(command_str) {
+        Ok(c) => c,
+        Err(_) => return Err("Unknown command\r\n".to_string()),
+    };
+    Ok((command, args[1..].to_vec()))
+}
