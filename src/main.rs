1091a3ef4936aa8e1615921dff5e872934cfae8b
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2024-03-12 08:04:08 -0300

    handshake pt 1

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use std::sync::Arc;
 // Uncomment this block to pass the first stage
 use anyhow::Result;
 
-use tokio::{net::TcpListener, sync::RwLock};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+    sync::RwLock,
+};
 
 mod args;
 mod client;
@@ -13,6 +17,9 @@ mod redis;
 async fn main() -> Result<()> {
     let args = args::Args::parse()?;
     let addr = format!("127.0.0.1:{}", args.port);
+    if let Some((host, port)) = &args.replica_of {
+        hand_shake(&host, *port).await?;
+    }
     let listener = TcpListener::bind(addr).await?;
     let redis = redis::Redis::new(args.port, args.replica_of);
     let redis = Arc::new(RwLock::new(redis));
@@ -28,6 +35,14 @@ async fn main() -> Result<()> {
             }
         });
     }
+}
+
+async fn hand_shake(host: &str, port: u16) -> Result<()> {
+    let mut stream = TcpStream::connect((host, port)).await?;
+    stream.write_all(b"*1\r\n$4\r\nPING\r\n").await?;
+    let mut buffer = [0; 128];
+    stream.read(&mut buffer).await?;
+    Ok(())
 }
 
 async fn start_expiration_thread(redis: Arc<RwLock<redis::Redis>>) {
