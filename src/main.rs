521ad06db335bfebf89cb5a4a5bcc54444f982c7
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2024-03-12 01:11:26 -0300

    pass 2st stage

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
