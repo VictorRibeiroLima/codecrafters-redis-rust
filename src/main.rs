11dd5d6860f1fb170be1c28cc64aef8df764a197
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2024-03-12 01:21:37 -0300

    pass 3st stage

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use anyhow::Result;
+use std::{
+    io::{BufRead, BufReader, Read, Write},
+    net::TcpListener,
+};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                stream.write(b"+PONG\r\n").unwrap();
+            Ok(stream) => {
+                handle_stream(stream)?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    Ok(())
+}
+
+fn handle_stream(mut stream: std::net::TcpStream) -> Result<()> {
+    let mut buf = [0; 512];
+    loop {
+        let n = stream.read(&mut buf)?;
+        if n == 0 {
+            break;
+        }
+        let message = std::str::from_utf8(&buf[..n]);
+        println!("received message: {:?}", message);
+        stream.write_all(b"+PONG\r\n")?;
+    }
+
+    Ok(())
 }
